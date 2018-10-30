3cfcd5887f1cdae74dcb4da9969003171652d243
nemanja <nemanja@massvision.net>
2018-10-30 13:28:41 +0100

    added multi entry

--- cmsitespec/Modules/SurveyNew/Common/InstallationSpecific.inc.php
+++ cmsitespec/Modules/SurveyNew/Common/InstallationSpecific.inc.php
@@ -5,3 +5,5 @@ require_once PATH_CORE . 'Modules/SurveyNew/Common/Parameters.inc.php';
 define('DM_ANKETA_MAIL_SENDER', 'noreply@dm-drogeriemarkt.rs');
 define('DM_ANKETA_MAIL_KOMENTAR', 'nagradni.konkurs@dm-drogeriemarkt.rs');
 define('DM_ANKETA_MAIL_BREND', 'nagradni.konkurs@dm-drogeriemarkt.rs');
+
+define('SURVEY_NEW_ALLOW_MULTI', true);
